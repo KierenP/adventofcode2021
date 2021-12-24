97f38b0ea739cc1f9eb566f6c983bf05cb24ee59
Kieren Pearson <kierenpearson9@gmail.com>
2021-12-24 21:46:26 +1100

    Improve runtime

--- Day_15/Part_2.py
+++ Day_15/Part_2.py
@@ -1,4 +1,7 @@
 
+from heapq import heappop
+from heapq import heappush
+
 risk = {}
 
 with open("Input.txt", "r") as file:
@@ -13,26 +16,21 @@ with open("Input.txt", "r") as file:
                 for b in range(0, 5):
                     risk[(x + size_x * a, y + size_y * b)] = sum(map(int, str(int(val) + a + b)))
 
-queue = {}
+queue = [(0, (0, 0))]
 complete = {}
-queue[(0, 0)] = 0
+visited = set()
 neighbours = { (-1, 0), (1, 0), (0, 1), (0, -1) }
 
 while len(queue) > 0:
-    next_node = min(queue, key=queue.get)
-    complete[next_node] = queue[next_node] + risk[next_node]
-    queue.pop(next_node)
+    dist, node = heappop(queue)
+    complete[node] = dist
 
     for neighbour in neighbours:
-        node = tuple(map(sum, zip(next_node, neighbour)))
+        next_node = (node[0] + neighbour[0], node[1] + neighbour[1])
 
-        if node in complete:
-            continue
-
-        if node[0] >= 0 and node[0] < size_x * 5 and node[1] >= 0 and node[1] < size_y * 5:
-            if node in queue:
-                queue[node] = min(queue[node], complete[next_node])
-            else:
-                queue[node] = complete[next_node]
+        if next_node[0] >= 0 and next_node[0] < size_x * 5 and next_node[1] >= 0 and next_node[1] < size_y * 5:
+            if next_node not in complete or complete[next_node] > dist + risk[next_node]:
+                complete[next_node] = dist + risk[next_node] 
+                heappush(queue, (dist + risk[next_node], next_node))  
 
 print(complete[(size_x*5-1, size_y*5-1)] - complete[(0, 0)])
